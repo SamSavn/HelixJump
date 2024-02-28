d1597b852f7e3d04085751d4d605b5942656e18b
Samuele Saviano <s.saviano@simultria.nl>
2024-02-28 18:23:11 +0100

    fix initial platforms positioning + prefabs and materials update

--- Assets/Scripts/Gameplay/Levels/Components/Tower.cs
+++ Assets/Scripts/Gameplay/Levels/Components/Tower.cs
@@ -14,25 +14,29 @@ namespace LKS.Gameplay
 #endregion
 
 #region Fields
-        private const int PLATFORMS_OFFSET = 3;
+        private const int ADDITIONAL_PLATFORMS_OFFSET = 3;
         private LevelGenerator _levelGenerator;
         private List<Platform> _platforms = new List<Platform>();
+
+        private float _globalPlatformsDistance;
         private float _position;
         private float _height;
-        #endregion
+#endregion
 
-        #region Properties
-        private float Offset => PLATFORMS_OFFSET * generationData.PlatformsDistance * 10f;
-        private float TopPosThreshold => _position + (_height * .5f) + Offset;
-        private float BottomPosThreshold => _position - (_height * .5f) - Offset; 
+#region Properties
+        private float Offset => ADDITIONAL_PLATFORMS_OFFSET * _globalPlatformsDistance;
+        private float TopPosThreshold => _position + _height + Offset;
+        private float BottomPosThreshold => _position - _height - Offset; 
 #endregion
 
 #region Unity Methods
         private void Awake()
         {
             _levelGenerator = new LevelGenerator(generationData);
+
+            _globalPlatformsDistance = transform.TransformPoint(new Vector3(0, generationData.PlatformsDistance, 0)).y;
             _position = transform.position.y;
-            _height = transform.localScale.y;
+            _height = transform.lossyScale.y;
 
             GameManager.SetTower(this);
         }
